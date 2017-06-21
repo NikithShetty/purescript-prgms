04239834e31e6e1f6f55af3ddeb1d7ee0e45802b
Nikith Shetty <shettynikith3@gmail.com>
2017-06-21 17:50:22 +0530

    Excersise5.5.2

--- src/Data/Excersise5.purs
+++ src/Data/Excersise5.purs
@@ -12,4 +12,11 @@ gcd n m = if n > m
 fact :: Int -> Int
 fact 0 = 1
 fact n = n * fact ( n - 1)
- 	
+
+--binomial coefficients
+binom :: Int -> Int -> Int
+binom n 0 = 1
+binom n k = 
+	if n == k
+		then 1
+		else (binom (n - 1) (k - 1)) + (binom (n - 1) k)  	
