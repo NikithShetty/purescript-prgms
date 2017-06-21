bc7fcae02c5a890c5ffe5be9b8ebcc4fcb6c983c
Nikith Shetty <shettynikith3@gmail.com>
2017-06-21 12:00:12 +0530

    Excersise 3.2

---
+++ test/MainTest.purs
@@ -0,0 +1,50 @@
+module Test.Main where
+
+import Prelude
+
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, logShow)
+import Data.AddressBook
+import Data.Maybe (Maybe)
+
+example :: Entry
+example =
+  { firstName: "John"
+  , lastName: "Smith"
+  , address: { street: "123 Fake St."
+             , city: "Faketown"
+             , state: "CA"
+             }
+  }
+
+example1 :: Entry
+example1 =
+  { firstName: "Paul"
+  , lastName: "Smulders"
+  , address: { street: "Fake St."
+             , city: "Faketown"
+             , state: "CA"
+             }
+  }
+
+book0 :: AddressBook
+book0 = emptyBook
+
+printEntry :: String -> String -> AddressBook -> Maybe String
+printEntry firstName lastName book = showEntry <$> findEntry firstName lastName book
+
+printEntryuStreet :: String -> AddressBook -> Maybe String
+printEntryuStreet street book = showEntry <$> findEntryuStreet street book
+
+main :: Eff (console :: CONSOLE) Unit
+main = do
+  let 
+    book1 = insertEntry example book0
+    book2 = insertEntry example1 book1
+
+  logShow "printEntry \"John\" \"Smith\" book0 -- "
+  logShow $ printEntry "John" "Smith" book0
+  logShow "printEntry \"John\" \"Smith\" book1 -- "
+  logShow $ printEntry "John" "Smith" book1
+  logShow "printEntryuStreet \"Fake St.\" book2 -- "
+  logShow $ printEntryuStreet "Fake St." book2
